b1c64c50aa9f510988bed62fa14c11a3afa011d5
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-24 15:16:01 +0100

    connected script.js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,23 @@ if (randomUserN > randomPcN) {
     pcNumber.append(randomPcN);
     diceResult.append("You Won!")
 }
+else {
+    console.log("The Pc Has Won This Time!");
+    userNumber.append(randomUserN);
+    pcNumber.append(randomPcN);
+    diceResult.append("The Pc Has Won This Time!")
+}
+
+console.log(randomUserN);
+console.log(randomPcN);
+
+
+//? Chiedi all’utente la sua email, 
+//? controlla che sia nella lista di chi può accedere 
+//? stampa un messaggio appropriato sull’esito del controllo
+const emailArray = ["pigpack@mail.it", "samsuing@gmail.com", "samuel@hotmail.com", "domenico@mail.net", "lorenzo@mail.com", "giulia@gmail.it", "mercedesbenz@live.net", "v8engine@gmail.it", "prietro@gmail.com", "giulio@live.it"]
+const button = document.getElementById('button-addon2')
+
+
 
 
