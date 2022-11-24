cde781235d9911c3c395a1b1f46690ecfb4ee930
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-24 15:09:39 +0100

    continuing exercise

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 //* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
 let randomUserN = Math.floor(Math.random() * 6);
 let randomPcN = Math.floor(Math.random() * 6);
+const userNumber = document.getElementById('userNumber');
+const pcNumber = document.getElementById('pcNumber');
+const diceResult = document.getElementById('diceResult');
+
+//* Stabilire il vincitore, in base a chi fa il punteggio più alto
+if (randomUserN > randomPcN) {
+    console.log("You Won!");
+    userNumber.append(randomUserN);
+    pcNumber.append(randomPcN);
+    diceResult.append("You Won!")
+}
+
 
